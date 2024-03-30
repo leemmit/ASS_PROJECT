fb5b00094434f575f0878d9cbceeaa89dd128065
leemmit <kolmykovl226@gmail.com>
2024-03-30 18:42:00 +0300

    Вроде починил но теперь тулбар белый

--- app/src/main/java/com/example/assproject/MainActivity.java
+++ app/src/main/java/com/example/assproject/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.assproject;
 
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
-import android.view.Menu;
+import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.widget.FrameLayout;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -16,14 +18,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.assproject.Adapter.ToDoAdapter;
 import com.example.assproject.Model.ToDoModel;
 import com.example.assproject.Utils.DataBaseHelper;
-import com.example.assproject.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import static com.example.assproject.R.id.home;
 
 public class MainActivity extends AppCompatActivity implements OnDialogCloseListener{
 
@@ -32,53 +33,33 @@ public class MainActivity extends AppCompatActivity implements OnDialogCloseList
     private DataBaseHelper myDB;
     private List<ToDoModel> mList;
     private ToDoAdapter adapter;
-    ActivityMainBinding binding;
-    private View home;
-    private View profile;
-    private View settings;
-    private Menu menu;
 
 
     @SuppressLint({"NonConstantResourceId", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        //setContentView(R.layout.activity_main);
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
 
-        replaceFragment(new Fragment());
+        //-----------------------------Bottom nav-------------------------------------------------
+        BottomNavigationView bottomNavigationView = findViewById(R.id.BottomNavView);
 
-        //home = (View) menu.getItem(R.id.home);
+        bottomNavigationView.setOnNavigationItemSelectedListener(menuItem -> {
 
-//        binding.BottomNavView.setOnItemSelectedListener(menuItem -> {
-//            switch (menuItem.getItemId()){
-//                case R.id.profile:
-//                    replaceFragment(new Fragment());
-//                    break;
-//
-//            }
-//
-//            return true;
-//        });
+            int itemID = menuItem.getItemId();
 
+            if (itemID == R.id.home){
+                replaceFragment(new HomeFragment(), false);
+            } else if (itemID == R.id.settings) {
+                replaceFragment(new SettingsFragment(), false);
+            } else { //profile
+                replaceFragment(new ProfileFragment(), false);
+            }
+            return true;
+        });
 
-//        profile = findViewById(R.id.profile);
-//        settings = findViewById(R.id.settings);
-
-//        binding.BottomNavView.setOnItemSelectedListener(item -> {
-//            switch (item.getItemId()){
-//                case R.:
-//
-//                    break;
-//                case profile:
-//                    break;
-//                case settings:
-//                    break;
-//
-//            }
-//            return true;
-//        });
+        replaceFragment(new HomeFragment(), true);
+        //-----------------------------Bottom nav--------------------------------------------------
 
         mRecycleView = findViewById(R.id.recycleview);
         fab = findViewById(R.id.fab);
@@ -113,10 +94,13 @@ public class MainActivity extends AppCompatActivity implements OnDialogCloseList
         adapter.notifyDataSetChanged();
     }
 
-    private void replaceFragment(Fragment fragment){
+    private void replaceFragment(Fragment fragment, boolean isAppInitialised){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout, fragment);
+
+        if (isAppInitialised) fragmentTransaction.add(R.id.frame_layout, fragment);
+        else fragmentTransaction.replace(R.id.frame_layout, fragment);
+
         fragmentTransaction.commit();
     }
 }
